f089d59c1d5fb32e798f02d78ec0db743774fec5
eugenia ong <eugenia.osy@gmail.com>
2019-05-07 16:46:40 -0700

    Add filepicker option

--- challenge_2/server/server.js
+++ challenge_2/server/server.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 
 const app = express();
 const port = 3000;
+const upload = multer();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());    // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));   // for parsing application/x-www-form-urlencoded
+
 app.use(express.static('client'));
 
-
-app.post('/upload_json', (req, res, next) => {
-  var data = JSON.parse(req.body.json);
+app.post('/upload_json', upload.single('jsonUpload'), (req, res, next) => {
+  var data = JSON.parse(String(req.file.buffer));
   var csv = parseIntoCSV(data);
 
   res.setHeader('Content-disposition', 'attachment; filename=default.csv');
   res.set('Content-type', 'text/csv');
-  res.send(csv);
+  res.status(200).send(csv);
+});
+
+app.post('/upload_text', (req, res, next) => {
+  var data = JSON.parse(req.body.json_text);
+  var csv = parseIntoCSV(data);
+
+  res.setHeader('Content-disposition', 'attachment; filename=default.csv');
+  res.set('Content-type', 'text/csv');
+  res.status(200).send(csv);
 });
 
 
@@ -40,7 +51,7 @@ const parseIntoCSV = (data) => {
 
     results.push(individual_details.join(','));
 
-    if (person.children.length) {
+    if (person.children && person.children.length) {
       person.children.forEach(person => (parseValues(person)));
     }
   }
