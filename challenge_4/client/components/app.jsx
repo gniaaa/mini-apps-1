c6a7484ba8829a9add1523ba18f1688c24cf7b71
eugenia ong <eugenia.osy@gmail.com>
2019-05-09 19:27:28 -0700

    refactor to handle sandwiched winning cases

--- challenge_4/client/components/app.jsx
+++ challenge_4/client/components/app.jsx
@@ -85,66 +85,83 @@ class App extends React.Component {
 
   checkWin(row, col, player) {
     var hasWon = false;
-    const checkE = (row, col, sum) => {
-      if (sum === 4) {
-        hasWon = true;
-        return;
-      } else {
-        if (col + 1 < 6 && this.state.board[row][col + 1] === player) {
-          checkE(row, col + 1, sum + 1);
-        }
-      }
-    };
 
-    const checkW = (row, col, sum) => {
-      if (sum === 4) {
-        hasWon = true;
-        return;
-      } else {
-        if (col - 1 >= 0 && this.state.board[row][col - 1] === player) {
-          checkW(row, col - 1, sum + 1);
-        }
+    const checkVerts = (row, col) => {
+      var sum = 1;
+      var u = row;
+      while (u - 1 >= 0 && this.state.board[u - 1][col] === player) {
+        sum++;
+        u--;
       }
-    };
 
-    const checkS = (row, col, sum) => {
-      if (sum === 4) {
+      if (sum >= 4) {
         hasWon = true;
-        return;
-      } else {
-        if (row - 1 >= 0 && this.state.board[row - 1][col] === player) {
-          checkS(row - 1, col, sum + 1);
-        }
       }
-    };
+    }
 
-    const checkSW = (row, col, sum) => {
-      if (sum === 4) {
+    const checkHors = (row, col) => {
+      var sum = 1;
+      var l = col;
+      while (l - 1 >= 0 && this.state.board[row][l - 1] === player) {
+        sum++;
+        l--;
+      }
+      var r = col;
+      while (r + 1 < 7 && this.state.board[row][r + 1] === player) {
+        sum++;
+        r++;
+      }
+      if (sum >= 4) {
         hasWon = true;
-        return;
-      } else {
-        if (row - 1 >= 0 && col - 1 >= 0 && this.state.board[row - 1][col - 1] === player) {
-          checkSW(row - 1, col - 1, sum + 1);
-        }
       }
-    };
+    }
 
-    const checkSE = (row, col, sum) => {
-      if (sum === 4) {
+    const checkMaj = (row, col) => {
+      var sum = 1;
+      var u = row;
+      var l = col;
+      while (u + 1 < 6 && l - 1 >= 0 && this.state.board[u + 1][l - 1] === player) {
+        sum++;
+        u++;
+        l++;
+      }
+      var d = row;
+      var r = col;
+      while (d - 1 >= 0 && r + 1 < 7 && this.state.board[d - 1][r + 1] === player) {
+        sum++;
+        d--;
+        r++;
+      }
+      if (sum >= 4) {
         hasWon = true;
-        return;
-      } else {
-        if (row - 1 >= 0 && col + 1 < 6 && this.state.board[row - 1][col + 1] === player) {
-          checkSE(row - 1, col + 1, sum + 1);
-        }
       }
-    };
+    }
 
-    checkE(row, col, 1);
-    checkW(row, col, 1);
-    checkS(row, col, 1);
-    checkSW(row, col, 1);
-    checkSE(row, col, 1);
+    const checkMin = (row, col) => {
+      var sum = 1;
+      var u = row;
+      var r = col;
+      while (u + 1 < 6 && r + 1 < 7 && this.state.board[u + 1][r + 1] === player) {
+        sum++;
+        u++;
+        r++;
+      }
+      var d = row;
+      var l = col;
+      while (d - 1 >= 0 && l - 1 >= 0 && this.state.board[d - 1][l - 1] === player) {
+        sum++;
+        d--;
+        l--;
+      }
+      if (sum >= 4) {
+        hasWon = true;
+      }
+    }
+
+    checkVerts(row, col);
+    checkHors(row, col);
+    checkMaj(row, col);
+    checkMin(row, col);
     return hasWon;
   }
 
