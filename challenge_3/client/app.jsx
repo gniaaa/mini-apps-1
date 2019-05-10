cceceecada9ca81e801e7051801088adb7c19cf3
eugenia ong <eugenia.osy@gmail.com>
2019-05-10 09:52:41 -0700

    Add challenge_3 (non webpack) version

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,18 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <button></button>
+      </div>
+    );
+  }
+}
 
 
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,23 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+const sequelize = new Sequelize('shopping_cart', 'root', 'eugenia', {
+  host: 'localhost',
+  dialect: 'mysql',
+});
+
+const
 
 
+
+
+
+  app.listen(port, () => console.log('listening on', port));
