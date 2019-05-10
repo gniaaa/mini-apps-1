80e0cf30c23254f721b24c4c4b26c911525715c9
eugenia ong <eugenia.osy@gmail.com>
2019-05-09 18:47:37 -0700

    implement working connect 4

--- challenge_4/client/components/app.jsx
+++ challenge_4/client/components/app.jsx
@@ -6,63 +6,213 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      board: Array(7).fill(Array(6).fill(null)),
+      board: [[null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null]],
       columns: Array(7).fill(0),
       currentPlayer: 'red',
     }
+
+    // this.checkValidMove = this.checkValidMove.bind(this);
+    // this.getCurrentRow = this.getCurrentRow.bind(this);
+    // this.updateBoard = this.updateBoard.bind(this);
+    // this.checkWin = this.checkWin.bind(this);
+    // this.updateCols = this.updateCols.bind(this);
+    // this.updateTurn = this.updateTurn.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.restartGame = this.restartGame.bind(this);
   }
 
-  checkWin(coord, player) {
-    // coord is [x, y] ==> [row, col]
-    // player is either 'red', 'black'
-    // check vertical
-
-    // check horizontal
-    // check major diag
-    // check minor diag
+  createEmptyBoard() {
+    return [[null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null]];
   }
 
-  // updates board arrays with 'red' or 'black'
-  // updates columns to next available row
-  // TODO: do not let the player add to col if row is at 7 -- ALL FULL
-  updateBoard(col, player) {
-    let nextAvailableRow = this.state.columns[col];
-    let updatedBoard = this.state.board.slice();
-    updatedBoard[nextAvailableRow][col] = player;
+  createEmptyColumns() {
+    return Array(7).fill(0);
+  }
 
+  restartGame() {
+    let newBoard = this.createEmptyBoard();
+    let newCols = this.createEmptyColumns();
+    this.setState({
+      board: newBoard,
+      columns: newCols
+    })
+  }
+
+  displayBoard() {
+    var board = [];
+    for (var row = 5; row >= 0; row--) {
+      for (var col = 0; col < 7; col++) {
+        board.push(<div className="tile" row={row} col={col}>
+          {this.state.board[row][col] === 'red' && <RedPiece />}
+          {this.state.board[row][col] === 'blue' && <BluePiece />}
+        </div>);
+      }
+    }
+    return board;
+  }
+
+  checkValidMove(col) {
+    if (this.state.columns[col] > 5) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  getCurrentRow(col) {
+    return this.state.columns[col];
+  }
+
+  updateBoard(row, col, player) {
+    let updatedBoard = this.state.board.slice(0);
+    updatedBoard[row][col] = player;
+
+    this.setState({
+      board: updatedBoard
+    })
+  }
+
+  checkWin(row, col, player) {
+    var hasWon = false;
+    const checkE = (row, col, sum) => {
+      if (sum === 4) {
+        hasWon = true;
+        return;
+      } else {
+        if (col + 1 < 6 && this.state.board[row][col + 1] === player) {
+          checkE(row, col + 1, sum + 1);
+        }
+      }
+    };
+
+    const checkW = (row, col, sum) => {
+      if (sum === 4) {
+        hasWon = true;
+        return;
+      } else {
+        if (col - 1 >= 0 && this.state.board[row][col - 1] === player) {
+          checkW(row, col - 1, sum + 1);
+        }
+      }
+    };
+
+    const checkS = (row, col, sum) => {
+      if (sum === 4) {
+        hasWon = true;
+        return;
+      } else {
+        if (row - 1 >= 0 && this.state.board[row - 1][col] === player) {
+          checkS(row - 1, col, sum + 1);
+        }
+      }
+    };
+
+    const checkSW = (row, col, sum) => {
+      if (sum === 4) {
+        hasWon = true;
+        return;
+      } else {
+        if (row - 1 >= 0 && col - 1 >= 0 && this.state.board[row - 1][col - 1] === player) {
+          checkSW(row - 1, col - 1, sum + 1);
+        }
+      }
+    };
+
+    const checkSE = (row, col, sum) => {
+      if (sum === 4) {
+        hasWon = true;
+        return;
+      } else {
+        if (row - 1 >= 0 && col + 1 < 6 && this.state.board[row - 1][col + 1] === player) {
+          checkSE(row - 1, col + 1, sum + 1);
+        }
+      }
+    };
+
+    checkE(row, col, 1);
+    checkW(row, col, 1);
+    checkS(row, col, 1);
+    checkSW(row, col, 1);
+    checkSE(row, col, 1);
+    return hasWon;
+  }
+
+
+  updateCols(col) {
     let updatedCols = this.state.columns.slice();
     updatedCols[col]++;
 
     this.setState({
-      board: updatedBoard,
       columns: updatedCols
     });
   }
 
-  // renderNewBoard() {
-  //   var board = [];
-  //   for (var row = 5; 5 >= 0; row--) {
-  //     for (var col = 0; col < 7; col--) {
-  //       board.push(<div className="tile" row={row} col={col}></div>)
-  //     }
-  //   }
-  //   return board;
-  // }
+  updateTurn() {
+    let nextPlayer = this.state.currentPlayer === 'red' ? 'blue' : 'red';
+    this.setState({
+      currentPlayer: nextPlayer
+    });
+  }
+
+  handleClick(e) {
+    let col = Number(e.target.getAttribute('col'));
+    let row = this.getCurrentRow(col);
+    if (!this.checkValidMove(col)) {
+      alert('Invalid move!');
+      return;
+    }
+
+    this.updateBoard(row, col, this.state.currentPlayer);
+    if (this.checkWin(row, col, this.state.currentPlayer)) {
+      alert(`congrats, ${this.state.currentPlayer} won!`);
+      this.restartGame();
+      return;
+    } else {
+      this.updateCols(col);
+      this.updateTurn();
+    }
+  }
 
 
   render() {
     return (
       <div>
+        <div id="title">CONNECT FOUR</div>
         <div id="insert">
           {this.state.columns.map((col, index) => {
-            return <div className="insert-tile" col={index}></div>
+            return (
+              <div className={`insert-tile ${this.state.currentPlayer}-insert`} col={index} onClick={this.handleClick}>
+              </div>
+            )
           })}
         </div>
-        <div id="board">
-        </div>
+        <div id="board">{this.displayBoard()}</div>
+        <div id="turn">current player: {this.state.currentPlayer}</div>
+        <div id="btn"><button id="newgame-btn" onClick={this.restartGame}>START NEW GAME</button></div>
       </div>
     );
   }
 };
+
+const RedPiece = (props) => (
+  <div className="red-piece" />
+);
+
+const BluePiece = (props) => (
+  <div className="blue-piece" />
+);
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
